49889899e34bab75382c0bf08d3930ce2b83cc98
Peter Cable <petercable@gmail.com>
2019-03-01 22:43:10 -0800

    add more metrics

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -57,13 +57,19 @@ stream_gauges = {
     'pf': Gauge('meter_power_factor_ratio', 'Power Factor', ['type', 'phase']),
 }
 
-
 production_gauges = {
     'activeCount': Gauge('production_active_count', 'Active Count', ['type']),
     'wNow': Gauge('power_now_watts', 'Active Count', ['type']),
     'whToday': Gauge('production_today_watthours', 'Total production today', ['type']),
     'whLastSevenDays': Gauge('production_7days_watthours', 'Total production last seven days', ['type']),
     'whLifetime': Gauge('production_lifetime_watthours', 'Total production lifetime', ['type']),
+}
+
+consumption_gauges = {
+    'wNow': Gauge('consumption_now_watts', 'Active Count', ['type']),
+    'whToday': Gauge('consumption_today_watthours', 'Total consumption today', ['type']),
+    'whLastSevenDays': Gauge('consumption_7days_watthours', 'Total consumption last seven days', ['type']),
+    'whLifetime': Gauge('consumption_lifetime_watthours', 'Total consumption lifetime', ['type']),
 }
 
 inverter_gauges = {
@@ -102,6 +108,15 @@ def scrape_production_json():
             value = each.get(key)
             if value is not None:
                 production_gauges[key].labels(type=mtype).set(value)
+    consumption = data['consumption']
+    print(consumption)
+    for each in consumption:
+        mtype = each['measurementType']
+        for key in ['wNow', 'whLifetime', 'whToday', 'whLastSevenDays']:
+            value = each.get(key)
+            if value is not None:
+                consumption_gauges[key].labels(type=mtype).set(value)
+
 
 
 def scrape_inverters():
